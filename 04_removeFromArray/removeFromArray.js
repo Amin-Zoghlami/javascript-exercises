b0c8f62b9fa551d97688b55099ad8840766ef875
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2024-12-15 23:20:51 -0800

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...vals) {
+    for (const val of vals) {
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] === val) {
+                list.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return list;
 };
 
 // Do not edit below this line
