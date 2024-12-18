b23dd2a9c98dac9f2e8a09e52fea9fa7b37e6c21
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2024-12-18 02:09:41 -0800

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let a = 0;
+    let b = str.length - 1;
+    while (a < b) {
+        let ch1 = str.charAt(a).toLowerCase();
+        let ch2 = str.charAt(b).toLowerCase();
+        
+        if ((ch1 < 'a' || 'z' < ch1) && (ch1 < '0' || '9' < ch1)) {
+            a++;
+            continue;
+        }
 
+        if ((ch2 < 'a' || 'z' < ch2) && (ch2 < '0' || '9' < ch2)) {
+            b--;
+            continue;
+        }
+
+        if (!(ch1 === ch2)) {
+            return false;
+        }
+
+        a++;
+        b--;
+    }
+    return true;
 };
 
 // Do not edit below this line
