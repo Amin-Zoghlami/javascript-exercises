e48c7286a0643166f64656a1fa0a9da4c06d3099
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2024-12-18 18:36:34 -0800

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    return people.reduce((prev, curr) => { 
+        
+        prevAge = prev.yearOfDeath === undefined 
+            ? date.getFullYear() - prev.yearOfBirth 
+            : prev.yearOfDeath - prev.yearOfBirth;
+        
+        currAge = curr.yearOfDeath === undefined 
+            ? date.getFullYear() - curr.yearOfBirth 
+            : curr.yearOfDeath - curr.yearOfBirth;
 
+        if (currAge > prevAge) {
+            return curr;
+        }
+
+        return prev;
+    }, {yearOfBirth: date.getFullYear()});
 };
 
 // Do not edit below this line
